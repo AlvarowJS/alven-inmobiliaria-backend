01f2024ee182d97ced2509400fe8c7c2f6637329
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2023-06-16 22:11:36 -0500

    evitando errores por valores nulos en pdf

--- resources/views/pdf/template.blade.php
+++ resources/views/pdf/template.blade.php
@@ -78,40 +78,45 @@
     <div class="container">
         <div class="header">
             <div class="header">
-                <h1 style="display: inline-block; margin-right: 90px">{{ $propiedades->publicidad->encabezado }}</h1>
+                <h1 style="display: inline-block; margin-right: 90px">{{ $propiedades->publicidad->encabezado ?? '' }}</h1>
                 <img src="{{ public_path('images/logo.png') }}" class="logo" alt="Logo" style="display: inline-block;">
             </div>
         </div>
         <div style="text-align: right; margin-bottom: 0;">
-        {{ $propiedades->direccion->calle }}  {{ $propiedades->direccion->numero }} - {{ $propiedades->direccion->municipio }} - {{ $propiedades->direccion->estado }}
+        {{ $propiedades->direccion->calle ?? '' }}  {{ $propiedades->direccion->numero ?? ''}} - {{ $propiedades->direccion->municipio ?? ''}} - {{ $propiedades->direccion->estado ?? ''}}
         </div>
         <table class="datos-propiedad">
             <tr class="basicos">
                 <td class="caracteristicas">
-                    Precio: {{ $propiedades->publicidad->precio_venta }}
-                    <p>{{ $propiedades->publicidad->descripcion }}</p>
+                    Precio: {{ $propiedades->publicidad->precio_venta ?? ''}}
+                    <p>{{ $propiedades->publicidad->descripcion ?? '' }}</p>
                     <p>Con las siguientes caracteristicas</p>
-                    <p>Mascotas: {{ $propiedades->caracteristica->mascotas}}</p>
+                    <p>Mascotas: {{ $propiedades->caracteristica->mascotas ?? ''}}</p>
                     <h2>Espacios:</h2>
-                    @php
-                        $espacios = json_decode($propiedades->caracteristica->espacios);
-                        $totalEspacios = count($espacios);
-                        $columna1Espacio = array_slice($espacios, 0, ceil($totalEspacios / 2));
-                        $columna2Espacio = array_slice($espacios, ceil($totalEspacios / 2));
-                    @endphp
-                       <div style="overflow: auto;">
-                            <div style="float: left; width: 50%;">
-                                @foreach ($columna1Espacio as $espacio)
-                                    <li>{{ $espacio }}</li>
-                                @endforeach
+                    @if (!empty($propiedades->caracteristica->espacios))
+                        @php
+                            $espacios = json_decode($propiedades->caracteristica->espacios);
+                            $totalEspacios = count($espacios);
+                            $columna1Espacio = array_slice($espacios, 0, ceil($totalEspacios / 2));
+                            $columna2Espacio = array_slice($espacios, ceil($totalEspacios / 2));
+                        @endphp
+                        <div style="overflow: auto;">
+                                <div style="float: left; width: 50%;">
+                                    @foreach ($columna1Espacio as $espacio)
+                                        <li>{{ $espacio }}</li>
+                                    @endforeach
+                                </div>
+                                <div style="float: left; width: 50%;">
+                                    @foreach ($columna2Espacio as $espacio)
+                                        <li>{{ $espacio }}</li>
+                                    @endforeach
+                                </div>
                             </div>
-                            <div style="float: left; width: 50%;">
-                                @foreach ($columna2Espacio as $espacio)
-                                    <li>{{ $espacio }}</li>
-                                @endforeach
-                            </div>
-                        </div>
+                    @else
+                            Sin Espacios
+                    @endif
                     <h2>Instalaciones:</h2>
+                    @if (!empty($propiedades->caracteristica->instalaciones))
                     @php
                     $instalaciones = json_decode($propiedades->caracteristica->instalaciones);
                     $totalInstalaciones = count($instalaciones);
@@ -131,8 +136,11 @@
                             @endforeach
                         </div>
                     </div>
-
+                    @else
+                        Sin Instalaciones
+                    @endif
                     <h2>Restricciones:</h2>
+                    @if (!empty($propiedades->caracteristica->restricciones))
                     @php
                     $restricciones = json_decode($propiedades->caracteristica->restricciones);
                     $totalRestricciones = count($restricciones);
@@ -152,7 +160,9 @@
                             @endforeach
                         </div>
                     </div>
-
+                    @else
+                        Sin Restricciones
+                    @endif
                 </td>
                 {{-- <td> --}}
 
@@ -168,21 +178,21 @@
         <h2 style="margin-top: 80px; margin-bottom: 0">Básicos</h2>
         <div style="overflow: auto; ">
             <div style="float: left; width: 40%;">
-                <p>Superficie del terreno: {{ $propiedades->basico->superficie_terreno }}</p>
-                <p>Superficie de construcción: {{ $propiedades->basico->superficie_construccion }}</p>
-                <p>Niveles construidos: {{ $propiedades->basico->niveles_construidos }}</p>
-                <p>Número de elevadores: {{ $propiedades->basico->numero_elevadores }}</p>
+                <p>Superficie del terreno: {{ $propiedades->basico->superficie_terreno ?? '' }}</p>
+                <p>Superficie de construcción: {{ $propiedades->basico->superficie_construccion ?? '' }}</p>
+                <p>Niveles construidos: {{ $propiedades->basico->niveles_construidos ?? '' }}</p>
+                <p>Número de elevadores: {{ $propiedades->basico->numero_elevadores ?? '' }}</p>
             </div>
             <div style="float: left; width: 30%;">
-                <p>Estacionamiento: {{ $propiedades->basico->estacionamiento }}</p>
-                <p>Cocinas: {{ $propiedades->basico->cocinas }}</p>
-                <p>Baños: {{ $propiedades->basico->banios }}</p>
-                <p>Medios Baños: {{ $propiedades->basico->medios_banios }}</p>
+                <p>Estacionamiento: {{ $propiedades->basico->estacionamiento ?? '' }}</p>
+                <p>Cocinas: {{ $propiedades->basico->cocinas ?? '' }}</p>
+                <p>Baños: {{ $propiedades->basico->banios ?? '' }}</p>
+                <p>Medios Baños: {{ $propiedades->basico->medios_banios ?? ''}}</p>
             </div>
             <div style="float: left; width: 30%;">
-                <p>Número de casas: {{ $propiedades->basico->numero_casas }}</p>
-                <p>Piso Ubicado: {{ $propiedades->basico->piso_ubicado }}</p>
-                <p>Recamaras: {{ $propiedades->basico->recamaras }}</p>
+                <p>Número de casas: {{ $propiedades->basico->numero_casas ?? ''}}</p>
+                <p>Piso Ubicado: {{ $propiedades->basico->piso_ubicado ?? ''}}</p>
+                <p>Recamaras: {{ $propiedades->basico->recamaras ?? '' }}</p>
 
             </div>
 
